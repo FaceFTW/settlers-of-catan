acf6a73862610b883f6a83a23c3b49a9bfcf7776
Alex Westerman <faceftw@outlook.com>
2023-04-24 22:13:51 -0400

    exchangeResources Complete

--- src/main/java/catan/Game.java
+++ src/main/java/catan/Game.java
@@ -1,6 +1,12 @@
 package catan;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+import catan.data.Player;
+import catan.data.ResourceType;
+import catan.data.TradeOffer;
 
 public class Game {
 	public static final int MAX_DICE_VALUE = 5;
@@ -9,10 +15,12 @@ public class Game {
 	private Random random;
 	private int currentTurn;
 	private int numberOfPlayers;
+	private List<Player> players;
 
 	public Game() {
 		this.random = new Random();
 		this.numberOfPlayers = DEFAULT_NUM_PLAYERS;
+		this.players = new ArrayList<>();
 	}
 
 	/**
@@ -51,11 +59,46 @@ public class Game {
 		}
 	}
 
+	public void exchangeResources(int fromPlayer, int toPlayer, TradeOffer offer) {
+		Player from = players.get(fromPlayer - 1);
+		Player to = players.get(toPlayer - 1);
+
+		from.modifyResource(ResourceType.WOOD, offer.getReceivedWood() - offer.getGivenWood());
+		from.modifyResource(ResourceType.BRICK, offer.getReceivedBrick() - offer.getGivenBrick());
+		from.modifyResource(ResourceType.SHEEP, offer.getReceivedSheep() - offer.getGivenSheep());
+		from.modifyResource(ResourceType.WHEAT, offer.getReceivedWheat() - offer.getGivenWheat());
+		from.modifyResource(ResourceType.ORE, offer.getReceivedOre() - offer.getGivenOre());
+
+		to.modifyResource(ResourceType.WOOD, offer.getGivenWood() - offer.getReceivedWood());
+		to.modifyResource(ResourceType.BRICK, offer.getGivenBrick() - offer.getReceivedBrick());
+		to.modifyResource(ResourceType.SHEEP, offer.getGivenSheep() - offer.getReceivedSheep());
+		to.modifyResource(ResourceType.WHEAT, offer.getGivenWheat() - offer.getReceivedWheat());
+		to.modifyResource(ResourceType.ORE, offer.getGivenOre() - offer.getReceivedOre());
+	}
+
+	/**************************************************
+	 * Getters and Setters
+	 **************************************************/
+
 	/**
 	 * @return Gets the current turn
 	 */
 	public int getTurn() {
 		return this.currentTurn;
+	}
+
+	/**
+	 * Gets the player with the given id
+	 *
+	 * @param playerId The ID of the player to get
+	 * @return The Player object with the given ID
+	 */
+	public Player getPlayer(int playerId) {
+		return players.get(playerId - 1);
+	}
+
+	public void addPlayer(Player p) {
+		this.players.add(p);
 	}
 
 }
---
+++ src/test/java/catan/game/PlayerResourceTests.java
@@ -0,0 +1,139 @@
+package catan.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import catan.Game;
+import catan.data.Player;
+import catan.data.ResourceType;
+import catan.data.ResourcesGroup;
+import catan.data.TradeOffer;
+
+public class PlayerResourceTests {
+	@Test
+	void exchangeResources_ExchangeOneofDifferentResources() {
+		Player player1 = new Player(1);
+		Player player2 = new Player(2);
+		for (ResourceType type : ResourceType.values()) {
+			player1.modifyResource(type, 5);
+			player2.modifyResource(type, 5);
+		}
+
+		Game game = new Game();
+		game.addPlayer(player1);
+		game.addPlayer(player2);
+
+		game.exchangeResources(1, 2, new TradeOffer(
+				new ResourcesGroup(1, 1, 0, 0, 0),
+				new ResourcesGroup(0, 0, 1, 1, 1)));
+
+		assertEquals(4, player1.getResourceCount(ResourceType.WOOD));
+		assertEquals(4, player1.getResourceCount(ResourceType.BRICK));
+		assertEquals(6, player1.getResourceCount(ResourceType.SHEEP));
+		assertEquals(6, player1.getResourceCount(ResourceType.WHEAT));
+		assertEquals(6, player1.getResourceCount(ResourceType.ORE));
+		assertEquals(6, player2.getResourceCount(ResourceType.WOOD));
+		assertEquals(6, player2.getResourceCount(ResourceType.BRICK));
+		assertEquals(4, player2.getResourceCount(ResourceType.SHEEP));
+		assertEquals(4, player2.getResourceCount(ResourceType.WHEAT));
+		assertEquals(4, player2.getResourceCount(ResourceType.ORE));
+	}
+
+	@Test
+	void exchangeResources_ExchangeOneofDifferentResources_OtherDirection() {
+		Player player1 = new Player(1);
+		Player player2 = new Player(2);
+		for (ResourceType type : ResourceType.values()) {
+			player1.modifyResource(type, 5);
+			player2.modifyResource(type, 5);
+		}
+
+		Game game = new Game();
+		game.addPlayer(player1);
+		game.addPlayer(player2);
+
+		game.exchangeResources(2, 1, new TradeOffer(
+				new ResourcesGroup(1, 1, 0, 0, 0),
+				new ResourcesGroup(0, 0, 1, 1, 1)));
+
+		assertEquals(6, player1.getResourceCount(ResourceType.WOOD));
+		assertEquals(6, player1.getResourceCount(ResourceType.BRICK));
+		assertEquals(4, player1.getResourceCount(ResourceType.SHEEP));
+		assertEquals(4, player1.getResourceCount(ResourceType.WHEAT));
+		assertEquals(4, player1.getResourceCount(ResourceType.ORE));
+		assertEquals(4, player2.getResourceCount(ResourceType.WOOD));
+		assertEquals(4, player2.getResourceCount(ResourceType.BRICK));
+		assertEquals(6, player2.getResourceCount(ResourceType.SHEEP));
+		assertEquals(6, player2.getResourceCount(ResourceType.WHEAT));
+		assertEquals(6, player2.getResourceCount(ResourceType.ORE));
+	}
+
+	@Test
+	void exchangeResources_ExchangeOneTypeOfResource() {
+		Player player1 = new Player(1);
+		Player player2 = new Player(2);
+		for (ResourceType type : ResourceType.values()) {
+			player1.modifyResource(type, 5);
+			player2.modifyResource(type, 5);
+		}
+		List<Player> players = new ArrayList<>();
+		players.add(player1);
+		players.add(player2);
+
+		Game game = new Game();
+		game.addPlayer(player1);
+		game.addPlayer(player2);
+
+		game.exchangeResources(1, 2, new TradeOffer(
+				new ResourcesGroup(2, 0, 0, 0, 0),
+				new ResourcesGroup(0, 0, 3, 0, 0)));
+
+		assertEquals(3, player1.getResourceCount(ResourceType.WOOD));
+		assertEquals(5, player1.getResourceCount(ResourceType.BRICK));
+		assertEquals(8, player1.getResourceCount(ResourceType.SHEEP));
+		assertEquals(5, player1.getResourceCount(ResourceType.WHEAT));
+		assertEquals(5, player1.getResourceCount(ResourceType.ORE));
+		assertEquals(7, player2.getResourceCount(ResourceType.WOOD));
+		assertEquals(5, player2.getResourceCount(ResourceType.BRICK));
+		assertEquals(2, player2.getResourceCount(ResourceType.SHEEP));
+		assertEquals(5, player2.getResourceCount(ResourceType.WHEAT));
+		assertEquals(5, player2.getResourceCount(ResourceType.ORE));
+	}
+
+	@Test
+	void exchangeResources_ExchangeOneTypeOfResource_OtherDirection() {
+		Player player1 = new Player(1);
+		Player player2 = new Player(2);
+		for (ResourceType type : ResourceType.values()) {
+			player1.modifyResource(type, 5);
+			player2.modifyResource(type, 5);
+		}
+		List<Player> players = new ArrayList<>();
+		players.add(player1);
+		players.add(player2);
+
+		Game game = new Game();
+		game.addPlayer(player1);
+		game.addPlayer(player2);
+
+		game.exchangeResources(2, 1, new TradeOffer(
+				new ResourcesGroup(2, 0, 0, 0, 0),
+				new ResourcesGroup(0, 0, 3, 0, 0)));
+
+		assertEquals(7, player1.getResourceCount(ResourceType.WOOD));
+		assertEquals(5, player1.getResourceCount(ResourceType.BRICK));
+		assertEquals(2, player1.getResourceCount(ResourceType.SHEEP));
+		assertEquals(5, player1.getResourceCount(ResourceType.WHEAT));
+		assertEquals(5, player1.getResourceCount(ResourceType.ORE));
+		assertEquals(3, player2.getResourceCount(ResourceType.WOOD));
+		assertEquals(5, player2.getResourceCount(ResourceType.BRICK));
+		assertEquals(8, player2.getResourceCount(ResourceType.SHEEP));
+		assertEquals(5, player2.getResourceCount(ResourceType.WHEAT));
+		assertEquals(5, player2.getResourceCount(ResourceType.ORE));
+	}
+
+}
