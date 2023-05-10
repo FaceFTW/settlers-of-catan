feabafc3ae4a4458bf0cd4b2020106feedfd3662
rhit-koenigjl <89945304+rhit-koenigjl@users.noreply.github.com>
2023-05-09 21:34:56 -0400

    doBankExchange test passes for player with insufficientMaterials

--- src/test/java/catan/game/BankExchangeTests.java
+++ src/test/java/catan/game/BankExchangeTests.java
@@ -8,8 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class BankExchangeTests {
     @Test
-    public void testFailingBankExchange_hasNoMaterials_returnsFalse() {
+    public void testBankExchange_hasNoMaterials_returnsFalse() {
         Game game = new Game();
         assertFalse(game.doBankExchange(1, ResourceType.ORE, ResourceType.BRICK));
+    }
+
+    @Test
+    public void testBankExchange_hasInsufficientMaterials_returnsFalse() {
+        Game game = new Game();
+        game.getPlayer(1).modifyResource(ResourceType.WOOD, 3);
+        assertFalse(game.doBankExchange(1, ResourceType.WOOD, ResourceType.WHEAT));
     }
 }
