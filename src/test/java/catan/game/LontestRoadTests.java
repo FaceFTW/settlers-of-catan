039dd72d4efc53bfeefc31f48c8540cfdff642ed
rhit-koenigjl <89945304+rhit-koenigjl@users.noreply.github.com>
2023-05-13 21:04:04 -0400

    updateLongestRoad test passes for board with four roads in a row

--- src/test/java/catan/game/LontestRoadTests.java
+++ src/test/java/catan/game/LontestRoadTests.java
@@ -14,5 +14,23 @@ public class LontestRoadTests {
         assertEquals(-1, b.getLongestRoadOwnerID());
     }
 
+    @Test
+    public void testUpdateLongestRoad_withRoadFourLong_returnsNegativeOne () {
+        Board b = new Board();
+
+        Coordinate[] c = new Coordinate[] {
+                new Coordinate(0, 1, 0),
+                new Coordinate(0, 2, 0),
+                new Coordinate(1, 2, 0),
+                new Coordinate(1, 1, 1)
+        };
+
+        for (int i = 1;i < c.length;i ++) {
+            b.createNewRoad(1, c[i - 1], c[i]);
+        }
+
+        b.updateLongestRoad();
+        assertEquals(-1, b.getLongestRoadOwnerID());
+    }
 
 }
