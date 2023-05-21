62e92ccf4cf4268ce98e74ca0d1e30f8bbf55665
rhit-koenigjl <89945304+rhit-koenigjl@users.noreply.github.com>
2023-05-20 21:49:30 -0400

    Road Build on road where start is not owned by player integration test passes

--- src/test/java/catan/game/RoadBuildIntegrationTests.java
+++ src/test/java/catan/game/RoadBuildIntegrationTests.java
@@ -96,4 +96,16 @@ public class RoadBuildIntegrationTests {
         assertEquals(0, player.get(0).getResourceCount(ResourceType.BRICK));
         assertEquals(0, player.get(0).getResourceCount(ResourceType.WOOD));
     }
+
+    @Test
+    void buildRoad_StartHasRoadNotOwnedByPlayer_ReturnsFalse() {
+        List<Player> player = createPlayerWithSettlementResources();
+        Board b = new Board();
+        b.createNewRoad(2, new Coordinate(1, 0, 0), new Coordinate(2, 0, 0));
+        Game g = new Game(b, player);
+
+        assertFalse(g.buildRoad(1,
+                new Coordinate(1, 0, 0),
+                new Coordinate(2, 0, 0)));
+    }
 }
