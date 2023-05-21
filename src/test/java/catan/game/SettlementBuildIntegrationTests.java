ec6659051003041393d67742a367c4b47334de14
rhit-koenigjl <89945304+rhit-koenigjl@users.noreply.github.com>
2023-05-20 22:32:16 -0400

    Build settlement where settlement is built on road not owned by player integration test passes

--- src/test/java/catan/game/SettlementBuildIntegrationTests.java
+++ src/test/java/catan/game/SettlementBuildIntegrationTests.java
@@ -2,6 +2,7 @@ package catan.game;
 
 import catan.data.Player;
 import catan.data.ResourceType;
+import catan.data.Road;
 import catan.data.Settlement;
 import catan.logic.Board;
 import catan.logic.Coordinate;
@@ -48,6 +49,20 @@ public class SettlementBuildIntegrationTests {
         Board b = new Board();
         b.createNewSettlement(new Coordinate(2, 2, 0),
                 player.get(0).getPlayerId());
+        Game game = new Game(b, player);
+
+        Coordinate c = new Coordinate(1, 0, 0);
+
+        assertFalse(game.buildSettlement(player.get(0).getPlayerId(), c, false));
+    }
+
+    @Test
+    void buildSettlement_ConnectedToARoadNotOwned_ReturnsFalse() {
+        List<Player> player = createPlayerWithSettlementResources();
+        Board b = new Board();
+        b.createNewSettlement(new Coordinate(2, 2, 0),
+                player.get(0).getPlayerId());
+        b.createNewRoad(2, new Coordinate(1, 0, 0), new Coordinate(2, 0, 0));
         Game game = new Game(b, player);
 
         Coordinate c = new Coordinate(1, 0, 0);
